e38c4bf6f3fa3ac48754df3281132db35eeec681
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2019-10-17 18:34:31 -0400

    made array an array

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -1,13 +1,13 @@
 function sumAll(arr) {
-  var max = Math.max(arr);
-  var min = Math.min(arr);
+  var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
  
   var count = 0;
-  for (var i = min; i < max; i++)
+  for (var i = min; i <= max; i++)
   {
   	count += i;
   }
-  console.log(count);
+  return count;
 }
 
 sumAll([1, 4]);
