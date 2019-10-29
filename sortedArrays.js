6eb3b986e0f6b069c86bbb333c0a8e32945c108a
AB <AndrewBraunsdorf@gmail.com>
2019-10-29 18:51:48 -0400

    sorted arrays with unknown arguments

---
+++ sortedArrays.js
@@ -0,0 +1,22 @@
+function uniteUnique(arr) {
+  // arr.sort();
+  let array = [];
+  
+  let i = 0;
+
+  while(arguments[i])
+  {
+    array = array.concat(arguments[i]);
+    i++
+  }
+
+  
+
+
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]); // [1, 3, 2, 5, 4]
+
+uniteUnique([1, 2, 3], [5, 2, 1]); // [1, 2, 3, 5]
+
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]); // [1, 2, 3, 5, 4, 6, 7, 8]
