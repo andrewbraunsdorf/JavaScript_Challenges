afa6d4bb7a1677258764fc5658f64ffe6d1b337c
AB <AndrewBraunsdorf@gmail.com>
2019-11-05 19:58:58 -0500

    added time

--- dropIt.js
+++ dropIt.js
@@ -19,3 +19,6 @@ dropElements([1, 2, 3], function(n) {return n > 0;}); // [1, 2, 3]
 dropElements([1, 2, 3, 4], function(n) {return n > 5;}); // []
 dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}); // [7, 4]
 dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}); // [3, 9, 2]
+
+
+//25
