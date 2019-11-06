28f116e3176867242c6367595470f64eb52c858d
AB <AndrewBraunsdorf@gmail.com>
2019-11-05 19:58:06 -0500

    works except when it should be blank

---
+++ dropIt.js
@@ -0,0 +1,20 @@
+function dropElements(arr, func) {
+  // arr.shift();
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  console.log(func(arr[0]));
+  console.log(arr);
+  return arr;
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}); // [3, 4]
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}); // 
+dropElements([1, 2, 3], function(n) {return n > 0;}); // [1, 2, 3]
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}); // []
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}); // [7, 4]
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}); // [3, 9, 2]
