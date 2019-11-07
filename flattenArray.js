76ba4ad20278f3aeca684e80be94cb33321f5651
AB <AndrewBraunsdorf@gmail.com>
2019-11-07 18:44:32 -0500

    flatten array

---
+++ flattenArray.js
@@ -0,0 +1,10 @@
+function steamrollArray(arr) {
+  let array = arr.flat(3);
+  console.log(array)
+  return array;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]); // [1, 2, 3, 4]
+steamrollArray([[["a"]], [["b"]]]); //["a", "b"]
+steamrollArray([1, [], [3, [[4]]]]); // [1, 3, 4]
+steamrollArray([1, {}, [3, [[4]]]]); // [1, {}, 3, 4]
