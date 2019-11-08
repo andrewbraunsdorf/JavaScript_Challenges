e5fb60ec7ea469d11824707f5a2218f9a938d086
AB <AndrewBraunsdorf@gmail.com>
2019-11-07 19:38:15 -0500

    put string into a variable to call for further use

--- binaryToString.js
+++ binaryToString.js
@@ -3,9 +3,14 @@ function binaryAgent(str) {
   str = str.replace(/\s+/g, '');
   str = str.match(/.{1,8}/g).join(" ");
 
- return str.split(" ").map(function (binaryToString) {
+ let string = str.split(" ").map(function (binaryToString) {
     return String.fromCharCode(parseInt(binaryToString, 2));
   }).join("");
+
+  // console.log(binaryToString);
+  console.log(string);
+  return string;
+
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"); // "Aren't bonfires fun!?"
